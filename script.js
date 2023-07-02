9b4799453859b421c8faa4f49a9b7f7ee872f5cb
allannhaka <119141267+allannhaka@users.noreply.github.com>
2023-07-02 12:41:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var slideIndex = 0;
+
+function showSlide() {
+  var slides = document.getElementsByClassName("slide");
+
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slideIndex++;
+
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+
+  setTimeout(showSlide, 2000);
+}
+
+window.onload = showSlide;
